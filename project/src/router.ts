2edba33c493f001bbd67cd054cbd0f108d3c95b0
pozhoushi <liuyi960422@163.com>
2022-07-10 18:49:57 +0800

    「 maybe i should focus on the content... 」

--- project/src/router.ts
+++
@@ -1,6 +0,0 @@
-import Front from './views/Front.vue'
-import js from './views/js.vue'
-export const routes = [
-  { path: '/', component: Front },
-  { path: '/js', component: js },
-]
--- project/src/views/Front.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="full-screen central">
-    <h1> Pick Your Poison </h1>
-  </div>
-</template>
